4611747787c4ff3c1bb7b9f01218ccc6b967b89c
Angelo Verlain <geoangercola@gmail.com>
2021-11-03 23:04:14 +0200

    render: include important meta tags in HTML template

--- packages/render/render.js
+++ packages/render/render.js
@@ -1,4 +1,8 @@
+let fs = require('fs');
+let path = require('path');
 let ejs = require('./lib/ejs.min.js');
+
+let templateFile = fs.readFileSync(path.resolve(__dirname, 'src', 'template.html'), 'utf8');
 
 let render = (page, templates, options = {}) => {
 	if (!page) throw "no page provided";
@@ -14,7 +18,11 @@ let render = (page, templates, options = {}) => {
 	// add lite youtube stuff
 	string = `<script type="module" src="https://unpkg.com/lite-youtube-embed/src/lite-yt-embed.js"></script><link rel="stylesheet" href="https://unpkg.com/lite-youtube-embed/src/lite-yt-embed.css"/>
 ` + string;
-	return string;
+	templateFile = templateFile
+		.replace('<!-- PAGE-TITLE -->', page.label + ' - rabbet')
+		.replace('<!-- PAGE-DESC -->', page.about)
+		.replace('<!-- PAGE-CONTENT -->', string);
+	return templateFile;
 }
 
 module.exports = render;
