2a25c1d2a7dd35ee75b22e2d71e53c4e180e4fac
Angelo Verlain <geoangercola@gmail.com>
2021-11-23 22:28:17 +0200

    db: include code to init db

---
+++ packages/db/init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQysbbCt4-eRhOlZifFuv6o_Aa0YifILw",
+  authDomain: "rabbetv4.firebaseapp.com",
+  projectId: "rabbetv4",
+  storageBucket: "rabbetv4.appspot.com",
+  messagingSenderId: "226169996755",
+  appId: "1:226169996755:web:6ca92d496834226ba35b26"
+};
+
+let init = () => initializeApp(firebaseConfig);
+
+export default init;
