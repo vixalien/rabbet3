2f717b48a5158767c91bb5790d53fb80916a14b9
Angelo Verlain <geoangercola@gmail.com>
2021-11-23 21:34:39 +0200

    pages: add support for 404

--- packages/pages/api/index.js
+++ packages/pages/api/index.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const {PassThrough} = require('stream');
 const express = require('express')
 var url = require('url');
 const app = express();
@@ -8,47 +10,57 @@ const firestore = require('../lib/firestore');
 const port = process.env.PORT || 3001;
 
 function fullUrl(req) {
-  return url.format({
-    protocol: req.protocol,
-    host: req.get('host'),
-    pathname: req.originalUrl
-  });
+	return url.format({
+		protocol: req.protocol,
+		host: req.get('host'),
+		pathname: req.originalUrl
+	});
 }
 
-app.get('/p/:username/:slug', async (req, res, next) => {
-  // let url = fullUrl(req);
-  // let subdomain = req.subdomains[0];
-  let username = req.params.username;
-  let slug = req.params.slug;
+// because on Vercel: ignored
+// app.use(express.static('public'));
 
-  // fetch user & page
-  // test username = vixalien
-  // test slug = test
-  let user = await firestore.query("users", ["username", "==", username]).then(e => e[0])
-  if (user) {
-    let page = await firestore.query("pages", ["user_uid", "==", user._id], ["slug", "==", slug]).then(e => e[0])
-    if (page) {
-      // render the page
-      res.setHeader('Content-Type', 'text/html');
-      return render(page.fields)
-        .then(html => res.send(html));
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
+app.get('/p/:username/:slug', async (req, res, next) => {
+	// let url = fullUrl(req);
+	// let subdomain = req.subdomains[0];
+	let username = req.params.username;
+	let slug = req.params.slug;
+
+	// fetch user & page
+	// test username = vixalien
+	// test slug = test
+	let user = await firestore.query("users", ["username", "==", username]).then(e => e[0])
+	if (user) {
+		let page = await firestore.query("pages", ["user_uid", "==", user._id], ["slug", "==", slug]).then(e => e[0])
+		if (page) {
+			// render the page
+			res.setHeader('Content-Type', 'text/html');
+			return render(page.fields)
+				.then(html => res.send(html));
+		} else {
+			next();
+		}
+	} else {
+		next();
+	}
 })
 
 app.get('/', (req, res) => {
-  // return to main page
-  res.send('Main Page!');
+	// return to main page
+	res.send('Main Page!');
 })
 
 app.use(function (req, res, next) {
-  res.status(404).send("404!")
+	const pass = new PassThrough();
+		const data = [];
+		pass.on('data', (chunk) => {
+				data.push(chunk)
+		});
+		http.request(req.protocol + '://' + req.get('host') + `/404.html`, (response) => {
+				response.pipe(pass).pipe(res);
+		}).end();
 })
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+	console.log(`App listening at http://localhost:${port}`)
 })
