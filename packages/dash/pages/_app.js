ca3f689d1caea48533c6f9510b60e9d4f011ae21
Angelo Verlain <geoangercola@gmail.com>
2021-12-04 12:10:06 +0200

    dash: set Rabbet as global HTML title

--- packages/dash/pages/_app.js
+++ packages/dash/pages/_app.js
@@ -44,6 +44,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
+				<title>Rabbet</title>
 				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				<link rel="shortcut icon" href="/favicon.ico" />
 			</Head>
